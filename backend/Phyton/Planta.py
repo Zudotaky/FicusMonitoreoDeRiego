47871937a2caade38f38eba236141117a516061d
Zudotaky <fire123@live.com.ar>
2019-10-01 17:56:50 -0300

    se modifica la relacion con espacio

--- backend/Phyton/Planta.py
+++ backend/Phyton/Planta.py
@@ -1,20 +1,22 @@
 
 from sqlalchemy import String, Column, Integer, ForeignKey
 from sqlalchemy.orm import relationship
+
+from backend.Phyton.Espacio import Espacio_planta
 from backend.Phyton.base import Base
 from backend.Phyton.Decoradores import jsonificable
 
 
 class Planta(Base, jsonificable):
 
-    __tablename__ = 'planta'
+    __tablename__ = 'plantas'
     id = Column(Integer, primary_key=True)
     descripcion = Column(String(300), nullable=False)
     nombre = Column(String(30), nullable=False)
-    espaciosId = relationship('Espacio', secondary='espacio_planta')
+    espaciosId = relationship('Espacio_planta', backref='planta',
+                             primaryjoin=id == Espacio_planta.planta_id)
 
-    def __init__(self, nombre, descripcion, id=None, espacios=[]):
+
+    def __init__(self, nombre, descripcion):
         self.nombre = nombre
         self.descripcion = descripcion
-        self.id = id
-        self.espacios = espacios
