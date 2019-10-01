5dbee5abd59580e6fba1d5e9fcf632bd5e6dd42a
Zudotaky <fire123@live.com.ar>
2019-10-01 14:53:15 -0300

    se modifica el dao creando una clase abstracta y dividiendo el dao a 3 subclases de este

--- backend/Phyton/DaoGeneral.py
+++ backend/Phyton/DaoGeneral.py
@@ -1,154 +1,74 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from backend.Phyton.base import Base
+
 from backend.Phyton.Espacio import Espacio
 from backend.Phyton.Planta import Planta
 from backend.Phyton.Registro import Registro
+from backend.Phyton.Decoradores import session, connecion
+
 
 class DaoGeneral:
 
-    engine = create_engine('sqlite:///libros.db', echo=True)
+    engine = create_engine('sqlite:///ficus.db', echo=True)
+    sessionConfig = sessionmaker(bind=engine)
     session = None
+    connector = None
 
-    #Funciones de sql
-    def levantarSession(self):
-        Session = sessionmaker(bind=engine)
-        self.session = Session()
 
-    def cerrarSession(self):
-        self.session.close()
+class DaoPlanta(DaoGeneral):
 
-    # #Funciones para inicializar la persistencia de objetos
-    # def persistirPlanta(self, planta):
-    #     self.connectarSql()
-    #     mycursor = self.mysqlConector.cursor()
-    #     mycursor.execute(self.queryGuardarPlanta, (planta.nombre(), planta.descripcion()))
-    #     self.mysqlConector.commit()
-    #     id = mycursor.lastrowid
-    #     planta.id(id)
-    #     self.cerrarSql()
-    #
-    #
-    # def persistirEspacio(self, espacio):
-    #     self.connectarSql()
-    #     registroDatos = (espacio.nombre(), espacio.descripcion(), espacio.tipiDeEspacio())
-    #     mycursor = self.mysqlConector.cursor()
-    #     mycursor.execute(self.queryGuardarEspacio, registroDatos)
-    #     self.mysqlConector.commit()
-    #     id = mycursor.lastrowid
-    #     espacio.id(id)
-    #     self.cerrarSql()
-    #
-    # def persistirRegistro(self, registro):
-    #     self.connectarSql()
-    #     registroDatos = (registro.plantaId(), registro.horarioDeRegistro(), registro.humedad(), registro.temperatura())
-    #     mycursor = self.mysqlConector.cursor()
-    #     mycursor.execute(self.queryGuardarSensos, registroDatos)
-    #     self.mysqlConector.commit()
-    #     self.cerrarSql()
-    #
-    #
-    #
-    # #funciones para Actualizar datos de objetos
-    #
-    #
-    #
-    # #Funciones para recuperar Objetos
-    # def obtenerPlantas(self):
-    #     self.connectarSql()
-    #     mycursor = self.mysqlConector.cursor()
-    #     mycursor.execute(self.queryRecuperarPlantas)
-    #     plantas = mycursor.fetchall()
-    #     self.cerrarSql()
-    #     plantas = self.formarPlantas(plantas)
-    #     return plantas
-    #
-    # def obtenerPlantasPorEspacioId(self, idEspacio):
-    #     self.connectarSql()
-    #     mycursor = self.mysqlConector.cursor()
-    #     datos = (idEspacio)
-    #     mycursor.execute(self.queryRecuperarPlantaPorEspacioId, datos)
-    #     plantas = mycursor.fetchall()
-    #     self.cerrarSql()
-    #     plantas = self.formarPlantas(plantas)
-    #     return plantas
-    #
-    # def obtenerPlantaPorId(self, id):
-    #     self.connectarSql()
-    #     mycursor = self.mysqlConector.cursor()
-    #     datos = (id)
-    #     mycursor.execute(self.queryRecuperarPlantaPorId, datos)
-    #     planta = mycursor.fetchall()
-    #     self.cerrarSql()
-    #     planta = self.formarPlantas(planta)
-    #     return planta
-    #
-    #
-    # def obtenerEspacio(self):
-    #     self.connectarSql()
-    #     mycursor = self.mysqlConector.cursor()
-    #     mycursor.execute(self.queryRecuperarEsapcios)
-    #     espacios = mycursor.fetchall()
-    #     self.cerrarSql()
-    #     espacios = self.formarEspacios(espacios)
-    #     return espacios
-    #
-    # def obtenerEspacioPorId(self, id):
-    #     self.connectarSql()
-    #     mycursor = self.mysqlConector.cursor()
-    #     datos = (id)
-    #     mycursor.execute(self.queryRecuperarEsapcioPorId, datos)
-    #     espacio = mycursor.fetchall()
-    #     self.cerrarSql()
-    #     espacio = self.formarEspacios(espacio)
-    #     return espacio
-    #
-    # def obtenerRegistroPorId(self, id):
-    #     self.connectarSql()
-    #     mycursor = self.mysqlConector.cursor()
-    #     mycursor.execute(self.querySelecionerRegistroPorId, (id))
-    #     registros = mycursor.fetchall()
-    #     self.cerrarSql()
-    #     registros = self.formarRegistros(registros)
-    #     return registros
-    #
-    # def obtenerSensosPorIdYFechas(self, id, fechaInicio, fechaFin):
-    #     self.connectarSql()
-    #     mycursor = self.mysqlConector.cursor()
-    #     mycursor.execute(self.querySelecionerRegistroPorIdYFechas, (id, fechaInicio, fechaFin))
-    #     registros = mycursor.fetchall()
-    #     self.cerrarSql()
-    #     registros = self.formarRegistros(registros)
-    #     return registros
+    @session
+    def persistirPlanta(self, planta):
+        self.session.add(planta)
+        return planta
+
+    @connecion
+    def obtenerPlantas(self):
+        plantas = self.session.query(Planta).all()
+        return plantas
+
+    @connecion
+    def obtenerPlantaPorId(self, id):
+        return self.session.query(Planta).filter(Planta.id == id).one()
+
+    @connecion
+    def obtenerPlantasPorEspacioId(self, espacioId):
+        return self.session.query(Planta).filter(Planta.espacios.id == espacioId).one()
 
 
 
 
 
-    #
-    # #funciones para crear objetos recuperados
-    # def formarPlantas(self, listaPlantas):
-    #     listaPlantasFormados = []
-    #     for datosPlanta in listaPlantas:
-    #         plantaRecuperada = Planta(datosPlanta[1], datosPlanta[2], datosPlanta[0], datosPlanta[3])
-    #         listaPlantasFormados.append(plantaRecuperada)
-    #     return listaPlantasFormados
-    #
-    # def formarEspacios(self, listaEspacios):
-    #     listaEspaciosFormados = []
-    #     for datosEspacio in listaEspacios:
-    #         plantaRecuperada = Espacio(datosEspacio[1], datosEspacio[2], datosEspacio[4], datosEspacio[3], datosEspacio[0])
-    #         listaEspaciosFormados.append(plantaRecuperada)
-    #     return listaEspaciosFormados
-    #
-    # def formarRegistros(self, listaRegistros):
-    #     listaRegistrosFormados = []
-    #     for datosRegistro in listaRegistros:
-    #         plantaRecuperada = Registro(datosRegistro[0], datosRegistro[1], datosRegistro[2], datosRegistro[3])
-    #         listaRegistrosFormados.append(plantaRecuperada)
-    #     return listaRegistrosFormados
+class DaoEspacio(DaoGeneral):
 
-engine = create_engine('sqlite:///ficus.db', echo=True)
+    @session
+    def persistirEspacio(self, espacio):
+        self.session.add(espacio)
+        return espacio
 
-Base.metadata.create_all(engine)
+    @connecion
+    def obtenerEspacio(self):
+        return self.session.query(Espacio).all()
+
+    @connecion
+    def obtenerEspacioPorId(self, id):
+        return self.session.query(Espacio).filter(Espacio.id == id).one()
+
+
+class DaoRegistro(DaoGeneral):
+
+    @session
+    def persistirRegistro(self, registro):
+        self.session.add(registro)
+        return registro
+
+    @connecion
+    def obtenerRegistroPorId(self, plantaId):
+        return self.session.query(Registro).filter(Registro.idPlanta == plantaId).all()
+
+    @connecion
+    def obtenerRegistrosPorIdYFechas(self, plantaId, fechaInicio, fechaFin):
+        return self.session.query(Registro).filter(Registro.plantaId == plantaId, Registro.fecha > fechaInicio, Registro.fecha < fechaFin).all()
+
+#Base.metadata.create_all(DaoGeneral.engine)
