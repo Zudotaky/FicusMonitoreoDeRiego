0da5a5da0cb7a7da28664c778f61ae9845c05621
patricio <patricio87209@gmail.com>
2019-10-24 21:32:34 -0300

    se muestran los espacios

--- web/src/componentes/Espacio.jsx
+++ web/src/componentes/Espacio.jsx
@@ -10,10 +10,15 @@ class Espacio extends Component{
 
     render(){
         const {id} = this.props
+        const {nombre} = this.props
+        const {descripcion} = this.props
 
         return(
   //          <div onClick={()=>handleClick(id)}>
-            <div>
+            <div className="card">
+                <img className="card-img-top" src={'https://cdn.homedit.com/wp-content/uploads/2016/08/DIY-pipe-plant-stand-300x250.jpg'} alt="PlantImage"/>
+                <h4 className="lineaDebajo"><b>{nombre}</b></h4>
+                <p>{descripcion}</p>
                 {id}
             </div>
         )
