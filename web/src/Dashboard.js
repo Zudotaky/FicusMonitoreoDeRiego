5094dd22a45f74decc860139b4650974697e481a
patricio <patricio87209@gmail.com>
2019-11-16 22:15:11 -0300

    Refactor de Dashboard

--- web/src/Dashboard.js
+++ web/src/Dashboard.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import ListaDePlantas from './componentes/ListaDePlantas';
-import ListaDeEspacios from './componentes/ListaDeEspacios';
+import React, { useState, useCallback, Children } from "react";
+import ListaDePlantas from "./componentes/ListaDePlantas";
+import ListaDeEspacios from "./componentes/ListaDeEspacios";
 import {
   Container,
   Row,
@@ -10,107 +10,76 @@ import {
   Card,
   CardText,
   CardTitle
-} from 'reactstrap';
-import Grafico from './componentes/Grafico';
+} from "reactstrap";
+import Grafico from "./componentes/Grafico";
 
 function Dashboard() {
-  const [espacios, setEspacios] = useState([])
-  const [plantas, setPlantas] = useState([])
-  const [espacioSeleccionado, setEspacioSeleccionado] = useState(null)
-  const [plantaSeleccionada, setPlantaSeleccionada] = useState(null)
-  const [dataPlanta, setDataPlanta] = useState({})
-
-  const [isOpenEspacios, setIsOpenEspacios] = useState(false)
-  const [isOpenPlantas, setIsOpenPlantas] = useState(false)
-  const [isOpenGrafico, setIsOpenGrafico] = useState(false)
-  const toggleEspacios = () => setIsOpenEspacios(!isOpenEspacios)
-  const togglePlantas = () => setIsOpenPlantas(!isOpenPlantas)
-  const toggleGrafico = () => setIsOpenGrafico(!isOpenGrafico)
+  const [espacios, setEspacios] = useState([]);
+  const [plantas, setPlantas] = useState([]);
+  const [espacioSeleccionado, setEspacioSeleccionado] = useState(null);
+  const [plantaSeleccionada, setPlantaSeleccionada] = useState(null);
+  const [dataPlanta, setDataPlanta] = useState({});
 
   return (
-        <div>
-          <Container fluid={true}>
-            <Col>
-              <Row className="listaCartas" md={3}>
-                <Card style={{ backgroundColor: 'lightgreen'}}>
-                  <CardBody>
-                    <CardTitle style={{ backgroundColor: 'lightgray' }}>
-                      <div
-                        onClick={toggleEspacios}
-                        style={{ backgroundColor: 'blue' }}
-                      >
-                        Espacios
-                      </div>
-                    </CardTitle>
-                    <CardText>
-                      <Collapse isOpen={isOpenEspacios}>
-                        <ListaDeEspacios
-                          handleClick
-                          espacios={espacios}
-                          setEspacios={setEspacios}
-                          espacioSeleccionado={espacioSeleccionado}
-                          setEspacioSeleccionado={setEspacioSeleccionado}
-                        />
-                      </Collapse>
-                    </CardText>
-                  </CardBody>
-                </Card>
-              </Row>
-              <Row className="listaCartas">
-                <Card style={{ backgroundColor: 'lightgreen' }}>
-                  <CardBody>
-                    <CardTitle style={{ backgroundColor: 'lightgray' }}>
-                      <div
-                        onClick={togglePlantas}
-                        style={{ backgroundColor: 'blue' }}
-                      >
-                        Plantas
-                      </div>
-                    </CardTitle>
-                    <CardText>
-                      <Collapse isOpen={isOpenPlantas}>
-                        <ListaDePlantas
-                          plantas={plantas}
-                          setPlantas={setPlantas}
-                          espacioSeleccionado={espacioSeleccionado}
-                          plantaSeleccionada={plantaSeleccionada}
-                          setPlantaSeleccionada={setPlantaSeleccionada}
-                          setDataPlanta={setDataPlanta}
-                          dataPlanta={dataPlanta}
-                        />
-                      </Collapse>
-                    </CardText>
-                  </CardBody>
-                </Card>
-              </Row>
-              <Row className="margenChart" style={{display: 'block'}} md={6}>
-                <Card style={{ backgroundColor: 'lightgreen' }}>
-                  <CardBody>
-                    <CardTitle style={{ backgroundColor: 'lightgray' }}>
-                      <div
-                        onClick={toggleGrafico}
-                        style={{ backgroundColor: 'blue' }}
-                      >
-                        Gráfico
-                      </div>
-                    </CardTitle>
-                    <CardText>
-                      <Collapse isOpen={isOpenGrafico}>
-                        <Grafico
-                          plantaSeleccionada={plantaSeleccionada}
-                          espacioSeleccionado={espacioSeleccionado}
-                          setDataPlanta={setDataPlanta}
-                          dataPlanta={dataPlanta}
-                        />
-                      </Collapse>
-                    </CardText>
-                  </CardBody>
-                </Card>
-              </Row>
-            </Col>
-          </Container>
-        </div>
-  )
+      <Container fluid>
+        <Col>
+          <Row className="listaCartas">
+            <CollapsableSection title="Espacios">
+              <ListaDeEspacios
+                handleClick
+                espacios={espacios}
+                setEspacios={setEspacios}
+                espacioSeleccionado={espacioSeleccionado}
+                setEspacioSeleccionado={setEspacioSeleccionado}
+              />
+            </CollapsableSection>
+          </Row>
+          <Row className="listaCartas">
+            <CollapsableSection title="Plantas">
+              <ListaDePlantas
+                plantas={plantas}
+                setPlantas={setPlantas}
+                espacioSeleccionado={espacioSeleccionado}
+                plantaSeleccionada={plantaSeleccionada}
+                setPlantaSeleccionada={setPlantaSeleccionada}
+                setDataPlanta={setDataPlanta}
+                dataPlanta={dataPlanta}
+              />
+            </CollapsableSection>
+          </Row>
+          <Row className="margenChart" style={{ display: "block"}}>
+            <CollapsableSection title="Gráfico">
+              <Grafico
+                plantaSeleccionada={plantaSeleccionada}
+                espacioSeleccionado={espacioSeleccionado}
+                setDataPlanta={setDataPlanta}
+                dataPlanta={dataPlanta}
+              />
+            </CollapsableSection>
+          </Row>
+        </Col>
+      </Container>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
+
+function CollapsableSection({ title, children }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleIsOpen = useCallback(() => {
+    setIsOpen(o => !o);
+  }, []);
+
+  return (
+    <Card style={{ backgroundColor: "lightgreen" }}>
+      <CardBody>
+        <CardTitle style={{ backgroundColor: "lightgray" }}>
+          <div onClick={toggleIsOpen} style={{ backgroundColor: "blue" }}>
+            {title}
+          </div>
+        </CardTitle>
+        <Collapse isOpen={isOpen}>{children}</Collapse>
+      </CardBody>
+    </Card>
+  );
+}
